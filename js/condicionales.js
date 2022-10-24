74d94f13393c2cefdcf967660ec096ad0f507179
Gabriel Anzola <gabrielanzola@gmail.com>
2022-10-24 17:25:40 -0300

    second commit

---
+++ js/condicionales.js
@@ -0,0 +1,10 @@
+let nombre = prompt('Ingrese su nombre').toLowerCase
+let pais = prompt('Ingrese su país').toLowerCase ()
+    if (pais == 'chile'){
+        console.log ('Ingreso correcto')
+        alert ('Hola bienvenido a nuestro sitio')
+    }
+    
+    else {
+        alert ('Nuestros servicios no están en ese país')
+    }
