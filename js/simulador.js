43436cbe40dcb80d30e7aa8db816d8a330cf7ea1
Gabriel Anzola <gabrielanzola@gmail.com>
2022-10-24 20:56:03 -0300

    simulador

---
+++ js/simulador.js
@@ -0,0 +1,10 @@
+let seleccionPropiedad = prompt('Seleccione una propiedad: Casa, Departamento, Terreno').toLowerCase()
+
+if (seleccionPropiedad == 'casa') {
+     console.log(nombre + ' seleccionaste una casa');
+ } else if (seleccionPropiedad == 'departamento') {
+     console.log(nombre + 'seleccionaste un departamento');
+ } else if (seleccionPropiedad == 'terreno') {
+     console.log(nombre +' seleccionaste un terreno');
+ } else {
+     console.log( nombre +' Ingreso un valor incorrecto');
